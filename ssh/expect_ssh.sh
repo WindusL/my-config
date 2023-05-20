d0716905b47d04466e8b8bbeaf4777af6f4fe9c8
Windus <huizhimrl@gmail.com>
2023-05-20 23:04:54 +0800

    修复 ssh 连接自定义端口号语法错误问题

--- ssh/expect_ssh.sh
+++ ssh/expect_ssh.sh
@@ -3,12 +3,12 @@
 #set password [lindex $argv 1 ]
 set server_protocol [lindex $argv 0]
 set server_prefix [lindex $argv 1]
-set timeout 3600                     
+set timeout 3600
 
 # 服务器列表
-dict set servers 前缀 "用户名@IP ### 密码"
-dict set servers 前缀 "用户名@IP ### 密码"
-dict set servers 前缀 "用户名@IP ### 密码"
+dict set servers 前缀 "[端口号 ### ]用户名@IP ### 密码"
+dict set servers 前缀 "[端口号 ### ]用户名@IP ### 密码"
+dict set servers 前缀 "[端口号 ### ]用户名@IP ### 密码"
 
 # 当我们使用字符串拆分字符串，tcl会使用字符串中的每个字符来拆分，无法达到我们的目的。
 # 可以使用这个过程进行分割
@@ -39,9 +39,10 @@ set server [dict get $servers $server_prefix]
 #set server_list [split $server]
 set server_list [OnePiece $server " ### "]
 
-spawn $server_protocol [lindex $server_list 0]
-expect {                  
-  "*yes/no" { send "yes\r"; exp_continue}    
-  "*password:" { send "[lindex $server_list 1]\r" }       
- }  
+spawn $server_protocol [expr [string equal $server_protocol "ssh"] == 1 ? {"-p"} : {"-P"}] [expr [llength $server_list] == 3 ? {[lindex $server_list 0]} : 22] [lindex $server_list end-1]
+
+expect {
+  "*yes/no" { send "yes\r"; exp_continue}
+  "*password:" { send "[lindex $server_list 1]\r" }
+}
 interact
